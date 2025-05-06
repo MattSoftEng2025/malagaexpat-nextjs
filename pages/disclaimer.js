2fc8d5eadd07b73b28b7d536929689d4f9a34dcd
Ali Gray <contactalig@protonmail.com>
2025-05-06 21:33:15 +0100

    Few build fixes.

--- pages/disclaimer.js
+++ pages/disclaimer.js
@@ -3,7 +3,16 @@ import Link from 'next/link'
 import API from '../utils/API'
 import { postedOrLastUpdatedText } from '../utils/helpers'
 
-export default function PrivateHealthcare({ page }) {
+export async function getStaticProps() {
+    const page = await API.getJson(`/pages/disclaimer`)
+    return {
+        props: {
+            page
+        },
+    }
+}
+
+export default function Disclaimer({ page }) {
     return (
         <>
             <Head>
@@ -23,18 +32,25 @@ export default function PrivateHealthcare({ page }) {
                         </div>
                     </div>
                 </div>
-                <section class="section">
-                    <div class="container">
-                        <div class="columns is-centered">
-                            <div class="column is-7-desktop">
-                                <div class="content is-medium">
-                                    <article>
-                                        <p class="intro">As an authorised Sanitas Broker, we at Malaga Expat Consulting can offer you a comprehensive range of Sanitas Health insurance covers. You can benefit from it both as a stand-alone health insurance in Spain or as a complementary product to your Spanish public health cover.</p>
-                                        <figure class="mx-auto">
-                                        </figure>
-                                        <p>Sanitas (part of BUPA) is Spain's second largest private health insurance provider and is definitely topping the ranking in terms of client's satisfaction and quality. Sanitas offers one of the most advanced networks of private hospitals and health professionals in Spain and abroad, as well as a very comprehensive range of private health covers to address most diverse needs.</p><p>And what counts most, Sanitas, unlike some other insurance providers, will never try to "phase you out" when you get older. Once you are in - you stay covered and protected.</p><p>As an obligatory immigration requirement for your Spanish residency permit, a comprehensive health insurance cover is to be maintained during the whole term of your residency in Spain.</p>
-                                        <p>Do get in touch to contract your Sanitas health insurance cover directly through Malaga Expat, whether you require a new cover or consider changing your current private health provider, we will be happy to supply a no obligation quote for a product best suitable for your needs.</p>                                        // </article></div><div class="columns"><div class="column is-4"><a class="button is-outlined is-dark is-fullwidth" href="/contact">Contact Malaga Expat ⟶</a></div><div class="column is-4">
-                                            <a class="button is-outlined is-dark is-fullwidth" href="/our-services">Back to services ⟶</a></div><div class="column is-4"><a class="button is-outlined is-dark is-fullwidth" href="/about">About ⟶</a>
+                <section className="section">
+                    <div className="container">
+                        <div className="columns is-centered">
+                            <div className="column is-7-desktop">
+                                <div className="content is-medium">
+                                    <article dangerouslySetInnerHTML={{ __html: page.content }}></article>
+                                </div>
+                                <div className="columns">
+                                    <div className="column is-4">
+                                        <Link href="/contact" className="button is-outlined is-dark is-fullwidth">Contact Malaga Expat ⟶
+                                        </Link>
+                                    </div>
+                                    <div className="column is-4">
+                                        <Link href="/our-services" className="button is-outlined is-dark is-fullwidth">View our services ⟶
+                                        </Link>
+                                    </div>
+                                    <div className="column is-4">
+                                        <Link href="/about" className="button is-outlined is-dark is-fullwidth">About ⟶
+                                        </Link>
                                     </div>
                                 </div>
                             </div>
