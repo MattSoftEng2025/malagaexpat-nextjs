897b199491854f3c3e92b3c405152a0215e08687
MattSoftEng2025 <matt@mattsofteng.com>
2025-09-03 19:34:07 +0200

    Update ping.js

--- pages/api/ping.js
+++ pages/api/ping.js
@@ -1,3 +1,9 @@
-export default function handler(req, res) {
-  res.status(200).json({ ok: true, method: req.method });
-}
+// pages/api/ping.js
+export default (req, res) => res.status(200).json({ ok: true, method: req.method });
+
+// pages/api/env-check.js
+export default (req, res) => res.status(200).json({
+  FROM_EMAIL: !!process.env.FROM_EMAIL,
+  TO_EMAIL: !!process.env.TO_EMAIL,
+  SENDGRID_API_KEY: !!process.env.SENDGRID_API_KEY
+});
