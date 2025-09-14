8ab7b76e6fccc5e9edca12e5cd43548304491c8f
Matthew Hillman <matt@mattsofteng.com>
2025-09-14 22:08:58 +0200

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -23,7 +23,7 @@ export default function Contact() {
 
         try {
             const res = await fetch(
-                'http://malagaexpat-frontend-env.eba-fjt7n9ad.eu-west-1.elasticbeanstalk.com//send_mail.php',
+                'https://malagaexpat-frontend-env.eba-fjt7n9ad.eu-west-1.elasticbeanstalk.com//send_mail.php',
                 {
                     method: 'POST',
                     body: JSON.stringify({ name, email, company, phone, content }),
